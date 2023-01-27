3d8eed050a4bbc66a89b365d20f528f2bfabf99c
Regie <reginapanina@mail.ru>
2023-01-27 17:10:04 +0300

    Main.class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000;
+        int bonus = 20;
+
+        int totalAmount = ticket * 1 / 20;
+        System.out.println(totalAmount);
+    }
+}
